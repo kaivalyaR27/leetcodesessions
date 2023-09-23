975a55e70566c6de6ff80062b15837e7bef9e9c4
kaivalya <kaivalya.kaivi>
2023-09-23 22:11:32 +0530

    palindrome

---
+++ pali.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class pali{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int rem,rev=0;
+int onum=n;
+while(n!=0){
+rem=n%10;
+rev=rev*10+rem;
+n/=10;
+}
+if(onum==rev)
+{
+System.out.println("YES");
+}
+else{
+System.out.println("N");
+}
+
