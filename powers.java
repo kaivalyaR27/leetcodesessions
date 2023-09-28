6661d405af720801ce5f2ffc1e5b4a873f7072c5
kaivalya <kaivalya.kaivi>
2023-09-28 10:41:51 +0530

    committed with correction of indentation

--- powers.java
+++ powers.java
@@ -1,13 +1,13 @@
 import java.util.*;
 import java.lang.Math;
 class powers{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter a base number");
-double x=sc.nextDouble();
-System.out.println("Enter the exponential number");
-double n=sc.nextDouble();
-double power=Math.pow(x,n);
-System.out.println(power);
-}
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a base number");
+        double x=sc.nextDouble();
+        System.out.println("Enter the exponential number");
+        double n=sc.nextDouble();
+        double power=Math.pow(x,n);
+        System.out.println(power);
+    }
 }
