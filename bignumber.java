189c550218b823a5f157150e688797bca6686855
kaivalya <kaivalya.kaivi>
2023-09-29 15:58:53 +0530

    finding biggest number among the given numbers

---
+++ bignumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class bignumber{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter first number");
+    int n1=sc.nextIn();
+    System.out.println("enter second number");
+    int n2=sc.nextInt();
+    System.out.println("enter third number");
+    int n3=sc.nextInt();
+    if(n1>n2 && n1>n3){
+        System.out.println("this is biggest number");
+    }
+    else if(n2>n1 && n2>n3){
+        System.out.println("this is biggest number");
+    }
+    else{
+        System.out.println("this is biggest number");
+    }
+    }
+
+}
