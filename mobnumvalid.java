e82deb3b0a8ea46b388cc6b451cf7804eaf2b96d
kaivalya <kaivalya.kaivi>
2023-10-04 12:23:08 +0530

    regex

---
+++ mobnumvalid.java
@@ -0,0 +1,20 @@
+import java.util.regex.*;
+import java.lang.*;
+import java.io.*;
+public class mobnumvalid
+{     
+public static boolean isvalidnum(String str)  
+{ 
+Pattern p = Pattern.compile("(0/91)?[0-9][0-9]{9}");  
+Matcher match = p.matcher(str);  
+return (match.find() && match.group().equals(str));  
+} 
+public static void main(String args[])  
+{        
+String str = "8688667047";    
+if (isvalidnum(str))  
+System.out.println("It is a valid mobile number.");   
+else  
+System.out.println("Entered mobile number is invalid.");      
+}  
+}   
