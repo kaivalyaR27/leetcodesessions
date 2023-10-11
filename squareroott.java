8bb473eba3c827a773c77871e7691fbfa940c9e9
kaivalya <kaivalya.kaivi>
2023-10-11 15:33:16 +0530

    modified squareroot

--- squareroott.java
+++ squareroott.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.lang.Math;
 class squareroott{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("enter a number");
-double x=sc.nextDouble();
-double y=Math.sqrt(x);
-System.out.println(y);
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        double x=sc.nextDouble();
+        double y=Math.sqrt(x);
+        System.out.println(y);
 }
 }
 
