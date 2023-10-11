bc038ffa4d7b261e45520dcde1ddd22771af5e07
kaivalya <kaivalya.kaivi>
2023-10-11 15:27:16 +0530

    modified heightchecker

--- heightcheck.java
+++ heightcheck.java
@@ -1,16 +1,14 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
+import java.util.Scanner;
 class heightcheck{
 public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("enter the height of the student");
-int n=sc.nextInt();
-if(n>=150){
-System.out.println("These are promoted to next grade");
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the height of the student");
+    int n=sc.nextInt();
+    if(n>=150){
+        System.out.println("These are promoted to next grade");
 }
 else{
-System.out.println("Better luck next time");
+    System.out.println("Better luck next time");
 }
 }
 }
