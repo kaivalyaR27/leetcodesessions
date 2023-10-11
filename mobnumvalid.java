1ec72ad15e8ce884f8bbd6934d827e5ebdf7cebc
kaivalya <kaivalya.kaivi>
2023-10-11 15:31:36 +0530

    modified regex

--- mobnumvalid.java
+++ mobnumvalid.java
@@ -1,20 +1,19 @@
-import java.util.regex.*;
-import java.lang.*;
-import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class mobnumvalid
 {     
-public static boolean isvalidnum(String str)  
+    public static boolean isvalidnum(String str)  
 { 
-Pattern p = Pattern.compile("(0/91)?[0-9][0-9]{9}");  
-Matcher match = p.matcher(str);  
-return (match.find() && match.group().equals(str));  
+    Pattern p = Pattern.compile("(0/91)?[0-9][0-9]{9}");  
+    Matcher match = p.matcher(str);  
+    return (match.find() && match.group().equals(str));  
 } 
 public static void main(String args[])  
 {        
-String str = "8688667047";    
-if (isvalidnum(str))  
-System.out.println("It is a valid mobile number.");   
-else  
-System.out.println("Entered mobile number is invalid.");      
+    String str = "8688667047";    
+    if (isvalidnum(str))  
+    System.out.println("It is a valid mobile number.");   
+    else  
+    System.out.println("Entered mobile number is invalid.");      
 }  
 }   
