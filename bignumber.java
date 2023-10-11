548424c1dd7a75d21eb4b8958d5ab0eb38b195fc
kaivalya <kaivalya.kaivi>
2023-10-11 15:25:45 +0530

    modified file

--- bignumber.java
+++ bignumber.java
@@ -1,6 +1,4 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 class bignumber{
 public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
