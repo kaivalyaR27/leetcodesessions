55ff2b8739c5b66092e6b2828f4c1fe293840c7c
kaivalya <kaivalya.kaivi>
2023-10-11 15:34:55 +0530

    program on sudoku

--- sudoku.java
+++ sudoku.java
@@ -1,6 +1,3 @@
-import java.io.*;
-import java.util.*;
-import java.lang.*;
 class sudoku{
 public static void main(String args[]){
 int[][]sudokugrid={
@@ -14,33 +11,34 @@ int[][]sudokugrid={
             {0, 0, 0, 4, 1, 9, 0, 0, 5},
             {0, 0, 0, 0, 8, 0, 0, 7, 9}
         };
-solvesudoku(sudokugrid);
-printsudoku(sudokugrid);
+        solvesudoku(sudokugrid);
+        printsudoku(sudokugrid);
 }
 public static boolean solvesudoku(int[][]grid){
-for(int i=0;i<9;i++)
-for(int j=0;j<9;j++)
-if(grid[i][j]==0)
-for(int n=1;n<=9;n++)
-if(isvalid(grid,i,j,n)){
-grid[i][j]=n;
-if(solvesudoku(grid))
-return true;
-grid[i][j]=0;
+
+    for(int i=0;i<9;i++)
+        for(int j=0;j<9;j++)
+        if(grid[i][j]==0)
+        for(int n=1;n<=9;n++)
+        if(isvalid(grid,i,j,n)){
+        grid[i][j]=n;
+        if(solvesudoku(grid))
+        return true;
+        grid[i][j]=0;
 }
 return true;
 }
 private static boolean isvalid(int[][]grid,int row,int col,int n){
-for(int x=0;x<9;x++)
-if(grid[row][x]==n || grid[x][col]==n || grid[row-row%3+x/3][col-col%3+x%3]==n)
-return false;
-return true;
+    for(int x=0;x<9;x++)
+    if(grid[row][x]==n || grid[x][col]==n || grid[row-row%3+x/3][col-col%3+x%3]==n)
+    return false;
+    return true;
 }
 public static void printsudoku(int[][]grid){
-for(int[]row:grid){
-for(int num:row)
-System.out.println(num+" ");
-System.out.println();
+    for(int[]row:grid){
+        for(int num:row)
+        System.out.println(num+" ");
+        System.out.println();
 }
 }
 }
